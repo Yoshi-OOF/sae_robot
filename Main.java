e721bc61fc60bd2d56e26175c4d956c1a30d3ae2
Yoshi <andreolienzo@gmail.com>
2023-10-24 09:24:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,22 @@ import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.Color;
 
 public class Main {
+	static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+	static EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
+	
+	public static void main(String[] args) {
+		suiviligne();
+		Robot20232024.FaireUneRotationAGauche(90);
+		Robot20232024.Arreter();
+		colorSensor.close();
+		sonar.close();
+	}
+	
 	public static void suiviligne() {
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
 		SensorMode color = colorSensor.getColorIDMode();
 		float[] sample = new float[color.sampleSize()];
 		String colorName;
 		
-		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
 		float[] value = new float[1];	
 		float  xDistance=(float) 0.9;
 		float  x=(float) 0.2;
@@ -27,27 +36,19 @@ public class Main {
 			colorName=Robot20232024.Namecolor(colorId);
 
 			if (colorName == "BLACK") {
-    			Robot20232024.AfficherUnmessageinst("debug color : " +colorName);
+    			Robot20232024.AfficherUnmessageinst("Color : " +colorName);
 				Robot20232024.AvancerMoteur(350,250);
-			}else if (colorName=="WHITE")    			{
-    			break;
+			}else if (colorName=="WHITE")  
+			{
+				Robot20232024.Arreter();
+				break;
 			}else
 			{
-				Robot20232024.AfficherUnmessageinst("debug color : " +colorName);
+				Robot20232024.AfficherUnmessageinst("Color : " +colorName);
 				Robot20232024.AvancerMoteur(250,350);
 			}
 			
 		}
-		
-		sonar.close();
-		colorSensor.close();
-		Robot20232024.Arreter();
-		Robot20232024.AfficherUnmessage("fin du programme");
-		Robot20232024.FaireUnBip();
-	}
-	
-	public static void main(String[] args) {
-		
 	}
 
 }
