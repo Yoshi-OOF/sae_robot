ecf134a96f517c2d84dee8d38a5cee523af7ec59
Yoshi <andreolienzo@gmail.com>
2023-10-25 13:09:26 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,56 +2,62 @@ package EV3;
 
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.robotics.Color;
 
 public class Main {
+
 	
 	public static void main(String[] args) {
 		suiviligne();
 		
 		Robot20232024.Tourner("C", 900, 270);
+
 		suiviligne();
+		// avancer reculer moteur A
+		while (true) {
+			Robot20232024.AvancerMoteur(250, 250);
+			Robot20232024.ReculerMoteur(250, 250);
+		}
+
+		Robot20232024.Arreter();
+
 	}
 	
 	public static void suiviligne() {
 		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
-		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
+		
 		SensorMode color = colorSensor.getColorIDMode();
 		float[] sample = new float[color.sampleSize()];
 		String colorName;
-		
-		float[] value = new float[1];	
-		float  xDistance=(float) 0.9;
 
-		while (xDistance > 0.2) {
-			sonar.getDistanceMode().fetchSample(value, 0);
-			xDistance= value[0];
+		while (true) {
 			color.fetchSample(sample, 0);
 			int colorId = (int)sample[0];
-			colorName=Robot20232024.Namecolor(colorId);
+			Robot20232024.AfficherUnmessageinst(colorId);
 
-			if (colorName == "BLACK") {
-    			Robot20232024.AfficherUnmessageinst("Color : " +colorName);
+			colorName=Robot20232024.Namecolor(colorId);
+			if (colorName.equals("BLACK")) {
 				Robot20232024.AvancerMoteur(350,250);
-			}else if (colorName=="WHITE")  
+				
+			}else if (colorName.equals("WHITE"))
 			{
 				Motor.B.setSpeed(0);
 				Motor.C.setSpeed(0);
 				break;
 			}else
 			{
-				Robot20232024.AfficherUnmessageinst("Color : " +colorName);
 				Robot20232024.AvancerMoteur(250,350);
 			}
 			
 		}
-
+		
 		colorSensor.close();
-		sonar.close();
 	}
 
 }
