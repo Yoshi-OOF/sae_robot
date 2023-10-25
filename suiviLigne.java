75a8793f70d8efc2f06dc3bbb196f11ee4d6dde6
Yoshi <andreolienzo@gmail.com>
2023-10-25 13:53:59 +0200

    Update suiviLigne.java

--- suiviLigne.java
+++ suiviLigne.java
@@ -1,49 +1,22 @@
-package EV3;
+	public static void suiviligne() {
+		EV3ColorSensor sensor4 = new EV3ColorSensor(SensorPort.S3);	
+		SampleProvider light4= sensor4.getMode("Red");	
+		double blanc = 0.5, color=0;
+		float sample[] = new float[light4.sampleSize()];
 
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.SensorMode;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.Color;
+		while (color < blanc) {
+			light4.fetchSample(sample, 0);
+			color = sample[0];	
 
-public class suiviLigne {
-
-    public static void main(String[] args) {
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
-		SensorMode color = colorSensor.getColorIDMode();
-		float[] sample = new float[color.sampleSize()];
-		String colorName;
+			if (color>0.06) {
+				Robot20232024.AvancerMoteur(250,350);
+			}else
+			{
+				Robot20232024.AvancerMoteur(350,250);
+			}
 			
-    		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
-    		float[] value = new float[1];	
-    		float  xDistance=(float) 0.9;
-    		float  x=(float) 0.2;
-
-    		while (xDistance > 0.2) {
-    			sonar.getDistanceMode().fetchSample(value, 0);
-    			xDistance= value[0];
-    			color.fetchSample(sample, 0);
-    			int colorId = (int)sample[0];
-    			colorName=Robot20232024.Namecolor(colorId);
-
-    			if (colorName == "BLACK") {
-	    			Robot20232024.AfficherUnmessageinst("debug color : " +colorName);
-    				Robot20232024.AvancerMoteur(350,250);
-    			}else if (colorName=="WHITE")    			{
-	    			break;
-    			}else
-    			{
-    				Robot20232024.AfficherUnmessageinst("debug color : " +colorName);
-    				Robot20232024.AvancerMoteur(250,350);
-    			}
-    			
-    		}
-    		
-    		sonar.close();
-    		colorSensor.close();
-    		Robot20232024.Arreter();
-    		Robot20232024.AfficherUnmessage("fin du programme");
-    		Robot20232024.FaireUnBip();
-    }
-}
+		}
+		Motor.B.stop();
+		Motor.C.stop();
+		sensor4.close();
+	}
