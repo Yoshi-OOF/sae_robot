ae6e8c8c88b083dd0c8e461eeff621d49ac581a1
Yoshi <andreolienzo@gmail.com>
2023-11-06 08:37:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -93,7 +93,13 @@ public class Main {
 		float distance;
 		distance = (float)0.9;
 		
-		while (distance > 0.3) {
+		int touche;
+		Robot20232024.AfficherUnmessageinst("Touche droite pour partir");
+		do{
+		touche = Robot20232024.Attendre();
+		}while(touche!=8);
+		
+		while (distance > 0.1) {
 			light4.fetchSample(sample, 0);
 			color = sample[0];	
 			sonar.getDistanceMode().fetchSample(value, 0);
@@ -179,57 +185,50 @@ public class Main {
 	
 	public static void passagesecret() {
 		// TODO Auto-generated method stub
-		//passer du programme de déplacement à celui là juste avant l'entrée du passage
-		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
-		float value[] = new float [1];
-		float distance;
-		distance = 9;
-		int i = 0;
-	
-		while (distance > 0.2) {
-			sonar.getDistanceMode().fetchSample(value, 0);
-			distance = value[0];
-			Robot20232024.AvancerMoteur(300,300);
-		}
-		 
-		sonar.close();
-		
-		EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
-		SensorMode touch1 = touchSensor1.getTouchMode();
-		float[] sample = new float[touch1.sampleSize()];	
-		
-		Robot20232024.Reculer();
-		do {
-			touch1.fetchSample(sample, 0);						
-		}while (sample[0] == 0);
-		touchSensor1.close();
-		
-		Robot20232024.FaireUnePause(10);
-		
-		for (i = 0; i < 1500;i++) {
-			Robot20232024.AvancerMoteur(500,500);
-		}
-		
-		Robot20232024.FaireUnePause(10);
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
-		SensorMode color = colorSensor.getColorIDMode();
-		float[] sample1 = new float[color.sampleSize()];
-		String colorName = "NONE";	
-		
-		while (colorName != "BLACK") {
-			color.fetchSample(sample1, 0);
-			int colorId = (int)sample1[0];
-			colorName=Robot20232024.Namecolor(colorId);
-			Motor.C.rotate(1);
-			Robot20232024.FaireUnePause(10);
-			
-		}
-		Robot20232024.FaireUnePause(10);
-		for (i= 0; i <1000; i++) {
-			Robot20232024.AvancerMoteur(1000,1000);
-		}
-		
-		colorSensor.close();
+				//passer du programme de déplacement à celui là juste avant l'entrée du passage
+				EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
+				float value[] = new float [1];
+				float distance;
+				distance = 9;
+				int i = 0;
+				
+				while (distance > 0.2) {
+					sonar.getDistanceMode().fetchSample(value, 0);
+					distance = value[0];
+					Robot20232024.AvancerMoteur(300,300);
+				}
+				 
+				sonar.close();
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(3000);
+				
+				EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
+				SensorMode touch1 = touchSensor1.getTouchMode();
+				float[] sample = new float[touch1.sampleSize()];	
+				
+				Robot20232024.Reculer();
+				do {
+					touch1.fetchSample(sample, 0);						
+				}while (sample[0] == 0);
+				touchSensor1.close();
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(2000);
+				
+				for (i = 0; i < 1000;i++) {
+					Robot20232024.AvancerMoteur(500,500);
+				}
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(2000);
+				
+				Robot20232024.FaireUneRotationADroite(50);
+				
+				Robot20232024.Arreter();
 	}
 	
 	public static void fin() {
