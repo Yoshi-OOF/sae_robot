3931de57d24c84e176ea64804d873053fec9988e
Yoshi <andreolienzo@gmail.com>
2023-11-06 08:36:00 +0100

    Update passageSecret.java

--- passageSecret.java
+++ passageSecret.java
@@ -11,62 +11,56 @@ import lejos.hardware.sensor.SensorMode;
 public class passageSecret {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		//passer du programme de déplacement à celui là juste avant l'entrée du passage
-		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
-		float value[] = new float [1];
-		float distance;
-		distance = 9;
-		int i = 0;
-		
-		int touche;
-		Robot20232024.AfficherUnmessageinst("Touche droite pour partir");
-		do{
-		touche = Robot20232024.Attendre();
-		}while(touche!=8);
-		
-		while (distance > 0.2) {
-			sonar.getDistanceMode().fetchSample(value, 0);
-			distance = value[0];
-			Robot20232024.AvancerMoteur(300,300);
-		}
-		 
-		sonar.close();
-		
-		Motor.B.stop();
-		Motor.C.stop();
-		Robot20232024.FaireUnePause(3000);
-		
-		EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
-		SensorMode touch1 = touchSensor1.getTouchMode();
-		float[] sample = new float[touch1.sampleSize()];	
-		
-		Robot20232024.Reculer();
-		do {
-			touch1.fetchSample(sample, 0);						
-		}while (sample[0] == 0);
-		touchSensor1.close();
-		
-		Motor.B.stop();
-		Motor.C.stop();
-		Robot20232024.FaireUnePause(2000);
-		
-		for (i = 0; i < 10000;i++) {
-			Robot20232024.AvancerMoteur(500,500);
-		}
-		
-		Motor.B.stop();
-		Motor.C.stop();
-		Robot20232024.FaireUnePause(2000);
-		
-		Robot20232024.FaireUneRotationADroite(50);
-		
-		i = 0;
-		Robot20232024.FaireUnePause(2000);
-		
-		for (i= 0; i <10000; i++) {
-			Robot20232024.AvancerMoteur(1000,1000);
-		}
+				EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
+				float value[] = new float [1];
+				float distance;
+				distance = 9;
+				int i = 0;
+				
+				int touche;
+				Robot20232024.AfficherUnmessageinst("Touche droite pour partir");
+				do{
+				touche = Robot20232024.Attendre();
+				}while(touche!=8);
+				
+				while (distance > 0.2) {
+					sonar.getDistanceMode().fetchSample(value, 0);
+					distance = value[0];
+					Robot20232024.AvancerMoteur(300,300);
+				}
+				 
+				sonar.close();
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(3000);
+				
+				EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
+				SensorMode touch1 = touchSensor1.getTouchMode();
+				float[] sample = new float[touch1.sampleSize()];	
+				
+				Robot20232024.Reculer();
+				do {
+					touch1.fetchSample(sample, 0);						
+				}while (sample[0] == 0);
+				touchSensor1.close();
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(2000);
+				
+				for (i = 0; i < 1000;i++) {
+					Robot20232024.AvancerMoteur(500,500);
+				}
+				
+				Motor.B.stop();
+				Motor.C.stop();
+				Robot20232024.FaireUnePause(2000);
+				
+				Robot20232024.FaireUneRotationADroite(50);
+				
+				Robot20232024.Arreter();
 		
 	}
 }
